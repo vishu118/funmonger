5c1926155e02b2864726612e68a5b7017a15a283
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-07-03 12:27:21 +0530

    header

--- src/components/JS/Header.js
+++ src/components/JS/Header.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Icon from "@mdi/react";
-import "../CSS/Header.css"
+import "../CSS/Header.css";
+import logo from "../IMAGES/movix-logo.png";
+import { mdiMagnify, mdiAccountCircle } from "@mdi/js";
 import {
-  mdiMagnify,
-  mdiYoutube,
-  mdiDotsVertical,
-  mdiAccountCircle,
-} from "@mdi/js";
-import { signInWithRedirect ,GoogleAuthProvider,getAuth} from "@firebase/auth";
+  signInWithRedirect,
+  GoogleAuthProvider,
+  getAuth,
+} from "@firebase/auth";
 import { app } from "../../Firebase";
 
 const provider = new GoogleAuthProvider();
@@ -15,34 +15,33 @@ const provider = new GoogleAuthProvider();
 const auth = getAuth(app);
 
 const Header = () => {
-
-    const handleSignIn = ()=>{
-        signInWithRedirect(auth, provider).then((res)=>{
-            console.log('sign in success')
-        }).catch((err)=>{
-            console.log('signin failed');
-        })
-    }
+  const handleSignIn = () => {
+    signInWithRedirect(auth, provider)
+      .then((res) => {
+        console.log("sign in success");
+      })
+      .catch((err) => {
+        console.log("signin failed");
+      });
+  };
   return (
     <>
       <div className="navbar">
         <div className="Navbar-logo">
-       
-         
-        <span className="title"><Icon path={mdiYoutube} size={1.5} color="red" /> FunMonger<sup >TM</sup></span>
-        
-      
+          <span className="title">
+            <img src={logo} alt="" className="logo" />
+            MOVIX
+          </span>
         </div>
 
         <div className="Navbar-search">
-          <input placeholder="Search" className="searchInput" />
+          <input placeholder="Search Here For Movie..." className="searchInput" />
           <button className="searchBtn">
             <Icon path={mdiMagnify} size={1} />
           </button>
-      
         </div>
         <div className="Navbar-account">
-          <Icon path={mdiDotsVertical} size={1} className="options" />
+
           <button className="signInBtn" onClick={handleSignIn}>
             <Icon path={mdiAccountCircle} size={1} color="RoyalBlue" />
             Sign In
